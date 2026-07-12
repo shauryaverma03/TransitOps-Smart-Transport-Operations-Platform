aa60308766c76a3d65fabf143babb3ceeecf5ddb
shauryaverma03 <shauryaverma036@gmail.com>
2026-07-12 10:22:11 +0530

    chore: prepare for production deployment (cors & vercel router)

--- server/index.js
+++ server/index.js
@@ -24,8 +24,18 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // Standard Middlewares
+const allowedOrigins = process.env.FRONTEND_URL 
+  ? [process.env.FRONTEND_URL, 'http://localhost:5173'] 
+  : ['http://localhost:5173'];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
